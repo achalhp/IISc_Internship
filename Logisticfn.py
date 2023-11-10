0c1c56ccb078df7ce872d87fef1e638c666a7560
Achal H P <achalhp@users.noreply.github.com>
2023-11-10 10:22:49 +0530

    Add files via upload

---
+++ Logisticfn.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+beta_coefficients = {
+    "β01": 0.1,
+    "β1": 0.5,
+    "β2": 0.5,
+    "β02": 1,
+    "β03": 0
+}
+
+data = {
+'X1': [2, 3, 5, 7, 1, 8, 4, 5, 6, 7],
+'X2': [1, 5, 3, 8, 2, 7, 5, 9, 4, 2],
+'Sero': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+}
+
+x1 = np.array(data["X1"])
+x2 = np.array(data["X2"])
+sero = np.array(data["Sero"])
+
+
+def V1(beta_coefficients, data):
+    x1 = np.array(data["X1"])
+    x2 = np.array(data["X2"])
+    sero = np.array(data["Sero"])
+    return beta_coefficients["β01"] + beta_coefficients["β1"] * x1 + beta_coefficients["β2"] * x2
+
+def V2(beta_coeffieicnts, data):
+    x1 = np.array(data["X1"])
+    x2 = np.array(data["X2"])
+    sero = np.array(data["Sero"])
+    return beta_coefficients["β02"] + beta_coefficients["β1"] * x1 + beta_coefficients["β2"] * x2
+
+def V3(beta_coefficients, data):
+    x1 = np.array(data["X1"])
+    x2 = np.array(data["X2"])
+    sero = np.array(data["Sero"])
+    return beta_coefficients["β03"] + beta_coefficients["β1"] * sero + beta_coefficients["β2"] * sero
+
+utilities = [V1(beta_coefficients, data), V2(beta_coefficients, data), V3(beta_coefficients, data)]
+
+def calculate_probabilities(parameters, data, utilities):
+    parameters = beta_coefficients
+    P_dict = {}
+    for i in range(len(utilities)):
+        P_dict['P{}'.format(i+1)] = np.exp(utilities[i]) / (np.exp(utilities[0]) + np.exp(utilities[1]) + np.exp(utilities[2]))
+    return P_dict
+
+P_dict = calculate_probabilities(beta_coefficients, data, utilities)
+
+with open('P_dict.txt', 'w') as file:
+    file.write(str(P_dict))
+
+
+# I'm avoiding for-loops to make it fast. Hence P_dict is outputted as is.
+
+# with open('P_dict.txt', 'w') as file:
+#     for key in P_dict.keys():
+#         line = str(key) + ': ' + ', '.join(map(str, P_dict[key])) + '\n'
+#         file.write(line)
